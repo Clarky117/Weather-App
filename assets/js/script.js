7fb77ad0ad77adc6d5f3e6c8f879a9672e485827
Clarky <clarky117@outlook.com>
2022-06-24 00:29:39 +0800

    psuedo code written, rough wireframe on html, api key added as variable

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// store api key in variable
+apiKey = '178a897e3808941aa91acb0d5fe8d92b'
+
+// get elements via document.get by id
+
+// get city searched for
+
+// call openweathermaps api to fetch the weather
+//  data for city searched for
+
+// return a promise and populate the DOM with data from this
+
+// store the city searched for in local storage and
+// append that to the search bar column
