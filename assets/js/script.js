4b323cffd5d6868264a1d8719e61021782a1d5fa
Clarky <clarky117@outlook.com>
2022-06-25 11:00:20 +0800

    html edits - further to be made once javascript written, javascript can receive user input

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,22 @@
 apiKey = '178a897e3808941aa91acb0d5fe8d92b'
 
 // get elements via document.get by id
+let searchFormEl = document.getElementById('search-form');
+let userInputEl = document.getElementById('user-search');
 
-// get city searched for
-
-// call openweathermaps api to fetch the weather
-//  data for city searched for
-
-// return a promise and populate the DOM with data from this
-
-// store the city searched for in local storage and
-// append that to the search bar column
+// get city searched for, when i click the search button
+searchFormEl.addEventListener('submit', function(event){
+    event.preventDefault(); 
+    // what is the users city?
+    let userInput = userInputEl.value;
+    // test
+    console.log(userInput);
+    
+    // call openweathermaps api to fetch the weather
+    //  data for city searched for
+    
+    // return a promise and populate the DOM with data from this
+    
+    // store the city searched for in local storage and
+    // append that to the search bar column
+});
