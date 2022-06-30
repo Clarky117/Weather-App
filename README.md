98fc221218a2f95ffa5cc0c28701536e8f15d6af
Clarky <clarky117@outlook.com>
2022-06-30 15:33:54 +0800

    created readme, added screenshot, refactored html and js

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # Weather-App
+
+In week 6 of the UWA Coding Bootcamp we have been tasked with creating a Weather App using the Open Weather Maps server-side API.<br><br>
+
+## Key Points
+
+As a traveller on the go it is imperitive to know the weather conditions of a particular place you are thinking of visiting so that you can plan for the future and know what to pack, or even if it is a place worth going to at the current time based on mother nature.
+<br><br>
+
+## Skills Used
+
+When creating this app the first element was designing the layout using bootstrap and the relevant classes to create a similar design and style to that of the mock up image.<br><br>
+
+Once this was done it was time to register an account on https://openweathermap.org/ and identify which API out of their numerous listed ones would be best suited. Unfortunately there was not a one size fits all solution, but there indeed was a solution. By initially searching a city in one API we can then store the latitude and longtitude as variable and pass them to another API call which then gives us all the relevant data that we need to populate the DOM.<br><br>
+
+Now that we can search for our city and fetch all the relevant data, it is time to give the nasty little uv index a background colour to distinguish its severity. I simply did this by writing a tidy little function with if/else statements, and pulled the colours from a simple google image search and using the colour dropper from the chrome dev tools to get the hex attribute.<br><br>
+
+When the user searches a city it is stored as a series of secondary buttons underneath the search element, and with the help of my tutor have made it so when you click on the button, you are again presented with the weather data of that city<br><br>
+
+## Site Visual
+
+![weather-app](/assets/images/weather-app.png)<br>
+
+
+## Things i want to work on
+
+The last 2 weeks have been somewhat of a huge learning curve for myself. Time management and destressing away from the vigors of this bootcamp are a must. I've been applying myself perhaps too hard in some areas which have led to a bit of burnout on my behalf, however now that i experienced the first assignment where i am not overly proud of my results it's time to get back on the horse and be better prepared for the upcoming weeks.
+
+Enough about the personal aspects, on the professional side i don't want to shy away from javascript with the hopes at some point it does become a bit easier. Talking to my rubber ducky on this one didn't help in the ways i hoped for. I need to review my use of for loops and implementing data through them. I feel like i am good at collaborating or working with a mentor/tutor to achieve the desired results but left to my own devices i struggle to get my head around certain aspects, especially because my rubber ducky does not answer me back!<br><br>
+
+## Links to Repository and Deployed Site
+
+- Repository - https://github.com/Clarky117/Weather-App
+- Live URL - https://clarky117.github.io/Weather-App/
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 // store api key in variable
 apiKey = '178a897e3808941aa91acb0d5fe8d92b';
 // get elements via document.get by id
+
 // form
 let searchFormEl = document.getElementById('search-form');
 let userInputEl = document.getElementById('user-search');
@@ -16,7 +17,6 @@ let currentUVEl = document.getElementById('current-uv');
 let fiveDayCardsEl = document.getElementById('five-day-cards');
 // search history element made with tutor Jain Dixit
 let searchHistoryEl = document.querySelector('.search-history');
-
 
 // write function to get uv index from one call api on openweathermaps
 function getOneCallApiUV(lon, lat) {
@@ -35,14 +35,12 @@ function getWeather(city) {
         .then(function (currentWeather) {
             let lon = currentWeather.coord.lon;
             let lat = currentWeather.coord.lat;
-            // return these parameters so we can get uv index
-            // from other api
+            // return these parameters so we can get uv index from other api 
             return getOneCallApiUV(lon, lat);
         });
 };
 
-
-// trying something here
+// function for background colour of UV index
 function uvBackgroundColour(uvIndex) {
     if (uvIndex <= 2) {
         currentUVEl.style.backgroundColor = "#4fb400";
@@ -55,72 +53,97 @@ function uvBackgroundColour(uvIndex) {
     } else if (uvIndex > 10) {
         currentUVEl.style.backgroundColor = "#998cff";
     };
-}
+};
 
 // trying to generate 5 day cards
-function generateFiveDayCards() {
+// function generateFiveDayCards() {
 
-    {/* <section class="card bg-light m-2">
-        <section class="card-body">
-            <h4 id="one-date" class="card-title">13/12/2012</h4>
-            <img src="#" alt="#">
+//     let fiveDayCards = document.createElement('section');
+//     fiveDayCards.setAttribute('class', 'card bg-light m-2');
+
+//     let fiveDayCardsBody = document.createElement('section');
+//     fiveDayCardsBody.setAttribute('class', 'card-body');
+
+//     let fiveDayh4 = document.createElement('h4');
+//     fiveDayh4.setAttribute('class', 'card-title');
+//     fiveDayh4.innerText = 'Date'
+
+//     let pTemp = document.createElement('p');
+//     pTemp.setAttribute('class', 'card-text');
+//     pTemp.innerText = 'Temp'
+
+//     let pWind = document.createElement('p');
+//     pWind.setAttribute('class', 'card-text');
+//     pWind.innerText = 'Wind'
+
+//     let pHumidity = document.createElement('p');
+//     pHumidity.setAttribute('class', 'card-text');
+//     pHumidity.innerText = 'Humidity'
+
+//     fiveDayCardsEl.appendChild(fiveDayCards);
+//     fiveDayCards.appendChild(fiveDayCardsBody);
+//     fiveDayCardsBody.appendChild(fiveDayh4, pTemp, pWind, pHumidity)
+
+
+{/* <section class="card bg-light m-2"> fiveDayCards
+            <section class="card-body"> fiveDayCardsBody
+                <h4 id="one-date" class="card-title">13/12/2012</h4> fiveDayh4
+                <img src="#" alt="#">
                 <p id="one-temp" class="card-text">Temp:</p>
                 <p id="one-wind" class="card-text">Wind:</p>
                 <p id="one-humidity" class="card-text">Humidity:</p>
-        </section>
-    </section> */}
+            </section>
+        </section> */}
 
-}
+// }
 
-function populateFiveDayCards(weatherData){
-
-}
+// function populateFiveDayCards(weatherData){
+// }
 
 // function to populate search history, written with the help of Jain Dixit in my tutoring session
-function populateSearchHistory(){
+function populateSearchHistory() {
     let searchHistoryItems = JSON.parse(localStorage.getItem('cities'));
 
-    if(searchHistoryItems){
+    if (searchHistoryItems) {
         for (let index = 0; index < searchHistoryItems.length; index++) {
-           const searchHistoryButton = document.createElement('button');
-           searchHistoryButton.innerText = searchHistoryItems[index];
-            
-           searchHistoryButton.addEventListener('click',getWeatherData );
-           searchHistoryEl.appendChild(searchHistoryButton);
-        }
-    }
-}
+            const searchHistoryButton = document.createElement('button');
+            searchHistoryButton.innerText = searchHistoryItems[index];
+
+            searchHistoryButton.setAttribute('class', 'btn btn-secondary d-flex flex-column my-3 w-100')
+
+            searchHistoryButton.addEventListener('click', getWeatherData);
+            searchHistoryEl.appendChild(searchHistoryButton);
+        };
+    };
+};
 
 populateSearchHistory()
 
-
-function getWeatherData (event) {
+function getWeatherData(event) {
     // console.log(event.target.innerText);
-   
+
     // what is the users city?
     let userInput = userInputEl.value;
 
-
     // code for local storage written with the help of tutor Jain Dixit 
-    if(event.type === 'submit'){
+    if (event.type === 'submit') {
         event.preventDefault();
         let searchHistoryItems = JSON.parse(localStorage.getItem('cities'));
-        if(searchHistoryItems){
-            if(!searchHistoryItems.includes(userInput)){
+        if (searchHistoryItems) {
+            if (!searchHistoryItems.includes(userInput)) {
                 searchHistoryItems.push(userInput);
-                localStorage.setItem('cities',JSON.stringify(searchHistoryItems));
+                localStorage.setItem('cities', JSON.stringify(searchHistoryItems));
             }
-        }else{
+        } else {
             let cityArray = [];
             cityArray.push(userInput);
-            localStorage.setItem('cities',JSON.stringify(cityArray));
+            localStorage.setItem('cities', JSON.stringify(cityArray));
         }
-    }else{
+    } else {
         userInput = event.target.innerText;
-    }
+    };
 
-    // call openweathermaps api to fetch the weather
-    //  data for city searched for
+    // call openweathermaps api to fetch the weather data for city searched for
     getWeather(userInput)
         .then(function (weatherData) {
             console.log(weatherData);
@@ -147,24 +170,9 @@ function getWeatherData (event) {
             // write function
             // populateFiveDayCards()
 
-            // write function
-            // store searches
-
-
-
-
-
             // populate 5 day forecast
-
-
-            // store the city searched for in local storage and
-            // append that to the search bar column
-            // localStorage.setItem(, userInput)   
-
         });
-}
-
-
+};
 
 // get city searched for, when i click the search button
 searchFormEl.addEventListener('submit', getWeatherData);
