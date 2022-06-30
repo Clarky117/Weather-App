794bcfb6e471176c2aacc2f58f1c00054dbddf55
Clarky <clarky117@outlook.com>
2022-06-30 13:13:49 +0800

    local storage added to javascript with help of tutor

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,16 +12,19 @@ let currentTempEl = document.getElementById('current-temp');
 let currentWindEl = document.getElementById('current-wind');
 let currentHumidityEl = document.getElementById('current-humidity');
 let currentUVEl = document.getElementById('current-uv');
+// five day forecast cards
+let fiveDayCardsEl = document.getElementById('five-day-cards');
+// search history element made with tutor Jain Dixit
+let searchHistoryEl = document.querySelector('.search-history');
+
 
 // write function to get uv index from one call api on openweathermaps
 function getOneCallApiUV(lon, lat) {
-    return fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&appid=${apiKey}`)
+    return fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=hourly,minutely&appid=${apiKey}`)
         .then(function (response) {
             return response.json();
         })
 };
-
-
 // write a function to get the weather data from api
 // https://openweathermap.org/current#name 
 function getWeather(city) {
@@ -40,7 +43,7 @@ function getWeather(city) {
 
 
 // trying something here
-function uvBackgroundColour(uvIndex){
+function uvBackgroundColour(uvIndex) {
     if (uvIndex <= 2) {
         currentUVEl.style.backgroundColor = "#4fb400";
     } else if (uvIndex <= 5 && uvIndex > 2) {
@@ -54,12 +57,68 @@ function uvBackgroundColour(uvIndex){
     };
 }
 
+// trying to generate 5 day cards
+function generateFiveDayCards() {
 
-// get city searched for, when i click the search button
-searchFormEl.addEventListener('submit', function (event) {
-    event.preventDefault();
+    {/* <section class="card bg-light m-2">
+        <section class="card-body">
+            <h4 id="one-date" class="card-title">13/12/2012</h4>
+            <img src="#" alt="#">
+                <p id="one-temp" class="card-text">Temp:</p>
+                <p id="one-wind" class="card-text">Wind:</p>
+                <p id="one-humidity" class="card-text">Humidity:</p>
+        </section>
+    </section> */}
+
+}
+
+function populateFiveDayCards(weatherData){
+
+}
+
+// function to populate search history, written with the help of Jain Dixit in my tutoring session
+function populateSearchHistory(){
+    let searchHistoryItems = JSON.parse(localStorage.getItem('cities'));
+
+    if(searchHistoryItems){
+        for (let index = 0; index < searchHistoryItems.length; index++) {
+           const searchHistoryButton = document.createElement('button');
+           searchHistoryButton.innerText = searchHistoryItems[index];
+            
+           searchHistoryButton.addEventListener('click',getWeatherData );
+           searchHistoryEl.appendChild(searchHistoryButton);
+        }
+    }
+}
+
+populateSearchHistory()
+
+
+function getWeatherData (event) {
+    // console.log(event.target.innerText);
+   
     // what is the users city?
     let userInput = userInputEl.value;
+
+
+    // code for local storage written with the help of tutor Jain Dixit 
+    if(event.type === 'submit'){
+        event.preventDefault();
+        let searchHistoryItems = JSON.parse(localStorage.getItem('cities'));
+        if(searchHistoryItems){
+            if(!searchHistoryItems.includes(userInput)){
+                searchHistoryItems.push(userInput);
+                localStorage.setItem('cities',JSON.stringify(searchHistoryItems));
+            }
+        }else{
+            let cityArray = [];
+            cityArray.push(userInput);
+            localStorage.setItem('cities',JSON.stringify(cityArray));
+        }
+    }else{
+        userInput = event.target.innerText;
+    }
+
     // call openweathermaps api to fetch the weather
     //  data for city searched for
     getWeather(userInput)
@@ -71,33 +130,29 @@ searchFormEl.addEventListener('submit', function (event) {
             // poppulate current city html
             currentDayCityDateIcon.innerHTML = `${userInputUpper} ${currentDate} <img src="http://openweathermap.org/img/wn/${currentWeatherIconCode}@2x.png">`;
             // current day attributes
-            currentTempEl.innerText = (weatherData.current.temp - 273.15).toFixed(2);
+            currentTempEl.innerText = weatherData.current.temp.toFixed(2);
             currentWindEl.innerText = weatherData.current.wind_speed + ' kms/h';
             currentHumidityEl.innerText = weatherData.current.humidity;
             // uv index
             let uvIndex = weatherData.current.uvi;
-            console.log(uvIndex)
+            // console.log(uvIndex)
 
             currentUVEl.innerText = uvIndex;
 
             uvBackgroundColour(uvIndex);
 
-            
+            // write function
+            // generateFiveDayCards()
 
-            // background color
-            // if loop for background colour of uv index
-            // write better parameters
-            // if (uvIndex <= 2) {
-            //     currentUVEl.style.backgroundColor = "#4fb400";
-            // } else if (uvIndex <= 5 && uvIndex > 2) {
-            //     currentUVEl.style.backgroundColor = "#f8b600";
-            // } else if (uvIndex <= 7 && uvIndex > 5) {
-            //     currentUVEl.style.backgroundColor = "#f85900";
-            // } else if (uvIndex <= 10 && uvIndex > 7) {
-            //     currentUVEl.style.backgroundColor = "#d81f1d";
-            // } else if (uvIndex > 10) {
-            //     currentUVEl.style.backgroundColor = "#998cff";
-            // };
+            // write function
+            // populateFiveDayCards()
+
+            // write function
+            // store searches
+
+
+
+
 
             // populate 5 day forecast
 
@@ -107,6 +162,9 @@ searchFormEl.addEventListener('submit', function (event) {
             // localStorage.setItem(, userInput)   
 
         });
-    // moved this to change scope
+}
 
-});
+
+
+// get city searched for, when i click the search button
+searchFormEl.addEventListener('submit', getWeatherData);
